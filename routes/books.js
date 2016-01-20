b5f19fd74e98d5a310dd2e6d93ea9679c4a13d36
Mike Wickwar <mike.wickwar@gmail.com>
2016-01-19 21:27:59 -0700

    bug 2 solved

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knexfile');
+var knex = require('../db/knex');
 
 function Books() {
   return knex('books');
@@ -30,7 +30,8 @@ router.get('/books/:id', function(req, res, next) {
 
 router.get('/books/:id/edit', function(req, res, next) {
   Books().where('id', req.params.id).then(function (book) {
-    res.render('books/edit', {book: book});
+    console.log(book);
+    res.render('books/edit', {books: book});
   });
 });
 
@@ -42,7 +43,7 @@ router.post('/books/:id', function (req, res, next) {
 
 router.post('/books/:id/delete', function (req, res, next) {
   Books().where('id', req.params.id).del().then(function (results) {
-    res.redirect('books', {books: results});
+    res.redirect('/books');
   })
 })
 
